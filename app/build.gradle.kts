9040a1f80e2b11888950d06c896354026736aab5
Jesse Salmi <jesse.s.salmi@proton.me>
2026-06-06 20:30:09 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.jss.smsotpextractor"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.3"
     }
 
     flavorDimensions += "modelDistribution"
